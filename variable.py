ee0faccb39c370960ba8853a859b2e71034ff09a
LKC5708 <epconc32@gmail.com>
2024-09-26 22:10:51 +0900

    3주차 퀴즈
    3주차 퀴즈 관련 코드

---
+++ variable.py
@@ -0,0 +1,16 @@
+#퀴즈 1번
+string = "Hello, World!"
+print(string[0:5])
+
+#퀴즈 2번
+result = (7 + 3) ** 2
+print(result)
+
+#퀴즈 3번
+A = [1, 2, 3, 4, 5]
+result = A[::-1]
+print(result)
+
+#퀴즈 4번
+fruits = {'apple':3, 'banana':5, 'cherry':2}
+print(fruits['banana'])
